acc66e050bc6f8d2e6d7a6d1d962f2f7565ef00a
diegodalairac <ddalairac@santadertecnologia.com.ar>
2021-03-17 11:24:18 -0300

    watch tsc && scss

--- js/main.js
+++ js/main.js
@@ -1,21 +1,16 @@
 "use strict";
 var canvas = document.getElementById("stage");
 var ctx = canvas.getContext("2d");
-// Full screen
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
-// Dibujar elemento
 var x = 0;
 function draw() {
     ctx.beginPath();
     ctx.rect(x, 20, 130, 200);
     ctx.stroke();
 }
-// Dibujar frame
 function frameLoop() {
-    // borrar canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // Dibujar canvas
     draw();
     if (x > canvas.width)
         x = -130;
